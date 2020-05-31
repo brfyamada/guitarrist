610b00bc3a6a50d7da758557b8a90efcdcfc3b88
Yamada <byamada@deloitte.com>
2020-05-31 14:42:39 -0300

    Primeiro deploy

---
+++ Guitarrist/Controllers/GuitarController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Guitarrist.Models.Entities;
+using Newtonsoft.Json;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Guitarrist.Controllers
+{
+    public class GuitarController : Controller
+    {
+        HttpClient client = new HttpClient();
+
+        public async Task<List<Guitar>> getGuitarAsync()
+        {
+            try
+            {
+
+                string url = "https://byamada.outsystemscloud.com/guitar_api/rest/guitarapi/guitars";
+                var response = await client.GetStringAsync(url);
+                var guitars = JsonConvert.DeserializeObject<List<Guitar>>(response);
+                return guitars;
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+
+  
+        public async Task<ActionResult> ListagemGuitar()
+        {
+            List<Guitar> guitars = new List<Guitar>();
+            guitars = await getGuitarAsync();
+
+            return View(guitars);
+        }
+       
+    }
+}
---
+++ Guitarrist/Models/Entities/Guitar.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Guitarrist.Models.Entities
+{
+    public class Guitar
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string model { get; set; }
+    }
+}
